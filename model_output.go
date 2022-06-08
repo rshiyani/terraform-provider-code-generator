d40b3b753b55f68ce391008573aa1979bd55e189
KrunalM-crest <krunal.mathukiya@crestdatasys.com>
2022-06-08 12:15:20 +0530

    removed map type attribute

--- model_output.go
+++ model_output.go
@@ -1,37 +1,42 @@
 package models
 
-import "encoding/json"
 
-type Contract struct {
-	TenantDn string   `json:"tenant_dn"`
-	Name     string   `json:"name"`
-	Prio     string   `json:"prio,omitempty"`
-	Filters  []Filter `json:"filter,omitempty"`
-}
 
-type Filter struct {
-	FilterName   string        `json:"filter_name"`
-	Id           string        `json:"id,omitempty"`
-	Description  string        `json:"description,omitempty"`
-	FilterEntrys []FilterEntry `json:"filter_entry,omitempty"`
-}
 
-type FilterEntry struct {
-	Casts           []string `json:"cast"`
-	FilterEntryName string   `json:"filter_entry_name"`
-	Id              string   `json:"id,omitempty"`
-	ApplyToFrag     string   `json:"apply_to_frag,omitempty"`
-}
+
+    type Contract struct {
+                        TenantDn string `json:"tenant_dn"`
+                        Name string `json:"name"`
+                        Prio string `json:"prio,omitempty"`
+                            Filters []Filter `json:"filter,omitempty"`
+    }
+
+                    type Filter struct {
+                        FilterName string `json:"filter_name"`
+                        Id string `json:"id,omitempty"`
+                        Description string `json:"description,omitempty"`
+                            FilterEntrys []FilterEntry `json:"filter_entry,omitempty"`
+    }
+
+                                    type FilterEntry struct {
+                            Casts []string `json:"cast"`
+                        FilterEntryName string `json:"filter_entry_name"`
+                        Id string `json:"id,omitempty"`
+                        ApplyToFrag string `json:"apply_to_frag,omitempty"`
+    }
+
+                
+
 
 func (contract *Contract) ToMap() (map[string]interface{}, error) {
-	contractJSON, err := json.Marshal(contract)
-	if err != nil {
-		return nil, err
-	}
-	contractMap := make(map[string]interface{})
-	err = json.Unmarshal(contractJSON, &contractMap)
-	if err != nil {
-		return nil, err
-	}
-	return contractMap, nil
+    contractJSON, err := json.Marshal(contract)
+    if err != nil {
+        return nil, err
+    }
+    contractMap := make(map[string]interface{})
+    err = json.Unmarshal(contractJSON, &contractMap)
+    if err != nil {
+        return nil, err
+    }
+    return contractMap, nil
 }
