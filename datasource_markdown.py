1bf3efa84b192930ef446ec2d4ac92c7322b024b
user1 <jay.sanghani@crestdatasys.com>
2022-06-15 14:46:18 +0530

    Resource and DataSource Markdown Added

---
+++ datasource_markdown.py
@@ -0,0 +1,17 @@
+from jinja2 import Environment, FileSystemLoader
+import yaml
+from utils import camelize, pascalize, snakify
+
+config = yaml.full_load(open('./config/resources/contract.yml'))
+env = Environment(loader=FileSystemLoader('./templates'),
+                  trim_blocks=True, lstrip_blocks=True)
+
+env.filters["camelize"] = camelize
+env.filters["pascalize"] = pascalize
+env.filters["snakify"] = snakify
+
+template = env.get_template('datasource_markdown.j2')
+
+# to save the results
+with open("./target/datasource.html.markdown", "w") as fh:
+    fh.write(template.render(config))
